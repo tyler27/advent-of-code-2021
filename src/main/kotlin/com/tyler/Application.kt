59fb89c73618ec3e052fe7218af2270e34285436
Tyler Telis <xlitersps@gmail.com>
2021-12-05 01:35:52 -0500

    Finished Day5

--- src/main/kotlin/com/tyler/Application.kt
+++ src/main/kotlin/com/tyler/Application.kt
@@ -4,25 +4,30 @@ import com.tyler.puzzle.day1.Day1
 import com.tyler.puzzle.day2.Day2
 import com.tyler.puzzle.day3.Day3
 import com.tyler.puzzle.day4.Day4
+import com.tyler.puzzle.day5.Day5
 
 /**
  * @author Tyler Telis
  * @email <xlitersps@gmail.com>
  */
 fun main() {
-    val day1 = Day1()
-    println("Answer one: ${day1.solvePartOne()}")
-    println("Answer two: ${day1.solvePartTwo()}")
+//    val day1 = Day1()
+//    println("Answer one: ${day1.solvePartOne()}")
+//    println("Answer two: ${day1.solvePartTwo()}")
+//
+//    val day2 = Day2()
+//    println("Answer one: ${day2.solvePartOne()}")
+//    println("Answer two: ${day2.solvePartTwo()}")
+//
+//    val day3 = Day3()
+//    println("Answer one: ${day3.solvePartOne()}")
+//    println("Answer two: ${day3.solvePartTwo()}")
+//
+//    val day4 = Day4()
+//    println("Answer one: ${day4.solvePartOne()}")
+//    println("Answer two: ${day4.solvePartTwo()}")
 
-    val day2 = Day2()
-    println("Answer one: ${day2.solvePartOne()}")
-    println("Answer two: ${day2.solvePartTwo()}")
-
-    val day3 = Day3()
-    println("Answer one: ${day3.solvePartOne()}")
-    println("Answer two: ${day3.solvePartTwo()}")
-
-    val day4 = Day4()
+        val day4 = Day5()
     println("Answer one: ${day4.solvePartOne()}")
     println("Answer two: ${day4.solvePartTwo()}")
 }
---
+++ src/main/kotlin/com/tyler/puzzle/day5/Day5.kt
@@ -0,0 +1,94 @@
+package com.tyler.puzzle.day5
+
+import com.tyler.puzzle.Puzzle
+
+/**
+ * @author Tyler Telis
+ * @email <xlitersps@gmail.com>
+ */
+class Day5 : Puzzle<Int> {
+    val numbersToPlot = input.map { it.trim().replace(" -> ", ",").split(",").map(String::toInt) }
+
+    override fun solvePartOne(): Int {
+        return plotLines(false).entries.count { it.value > 0 }
+    }
+
+    override fun solvePartTwo(): Int {
+        return plotLines(true).entries.count { it.value > 0 }
+    }
+
+    fun plotLines(includeVerticalLines: Boolean): MutableMap<Pair<Int, Int>, Int> {
+        val lines = mutableMapOf<Pair<Int, Int>, Int>()
+
+        numbersToPlot.map {
+            val (x1, y1, x2, y2) = it
+
+            if (x1 == x2) plotHorizontalLine(lines, x1, y1, y2)
+            else if (y1 == y2) plotVerticalLine(lines, x1, y1, x2)
+            else if (includeVerticalLines) plotVerticalLines(lines, x1, y1, x2, y2)
+        }
+
+        return lines
+    }
+
+    private fun plotVerticalLines(
+        lines: MutableMap<Pair<Int, Int>, Int>,
+        x1: Int,
+        y1: Int,
+        x2: Int,
+        y2: Int
+    ) {
+        var x = x1
+        var y = y1
+
+        while (true) {
+            val point = x to y
+            plotLine(lines, point)
+
+            if (x == x2)
+                break
+
+            x = if (x2 > x) ++x else --x
+            y = if (y2 > y) ++y else --y
+        }
+    }
+
+    private fun plotVerticalLine(
+        lines: MutableMap<Pair<Int, Int>, Int>,
+        x1: Int,
+        y1: Int,
+        x2: Int
+    ) {
+        val minX = x1.coerceAtMost(x2)
+        val maxX = x1.coerceAtLeast(x2)
+
+        (minX..maxX).forEach { x ->
+            val point = x to y1
+            plotLine(lines, point)
+        }
+    }
+
+    private fun plotHorizontalLine(lines: MutableMap<Pair<Int, Int>, Int>, x1:Int, y1: Int, y2: Int) {
+        val minY = y1.coerceAtMost(y2)
+        val maxY = y1.coerceAtLeast(y2)
+
+        (minY..maxY).forEach { y ->
+            val point = x1 to y
+            plotLine(lines, point)
+        }
+    }
+
+    private fun plotLine(
+        lines: MutableMap<Pair<Int, Int>, Int>,
+        point: Pair<Int, Int>
+    ) {
+        if (!lines.contains(point)) lines[point] = -1
+        lines[point] = lines[point]!! + 1
+    }
+
+
+    private companion object {
+        const val INPUT_FILE = "/day5/input.txt"
+        val input = Day5::class.java.getResourceAsStream(INPUT_FILE)!!.bufferedReader().readLines()
+    }
+}
